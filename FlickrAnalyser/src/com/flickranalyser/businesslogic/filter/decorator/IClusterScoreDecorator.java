67b376087291da11da41df0afe25301e5114dbb5
dvallesvalls <dvalles@web.de>
2014-07-25 20:52:37 +0200

    DVV Refactoring

--- FlickrAnalyser/src/com/flickranalyser/businesslogic/filter/decorator/IClusterScoreDecorator.java
+++
@@ -1,9 +0,0 @@
-package com.flickranalyser.businesslogic.filter.decorator;
-
-import com.flickranalyser.pojo.Cluster;
-
-public interface IClusterScoreDecorator {
-
-	
-	double scoreCluster(Cluster clusterToScore);
-}
