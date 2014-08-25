98f7f0c0ed0c78455638a119ef0cadaea557baa5
Constantin Scheuermann <cscheuermann@t-online.de>
2014-08-25 21:13:22 +0200

    Session Simon Consti

---
+++ iOS-App-Swift/SEEKRET-SWIFT/ViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ViewController.swift
+//  SEEKRET-SWIFT
+//
+//  Created by Constantin Scheuermann on 8/25/14.
+//  Copyright (c) 2014 Constantin Scheuermann. All rights reserved.
+//
+
+import UIKit
+
+
+class ViewController: UIViewController{
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+               
+        
+        
+    
+        
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
